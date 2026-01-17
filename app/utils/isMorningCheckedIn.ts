2124994c0ce68a5ec62e7f155779f318d8c57b63
Begaiym Adylbek kyzy <mrmldsky27@gmail.com>
2026-01-17 16:33:56 +0100

    Fix isMorningCheckedIn calculations

--- app/utils/isMorningCheckedIn.ts
+++ app/utils/isMorningCheckedIn.ts
@@ -2,11 +2,11 @@ import { MorningCheckin } from "@/app/types";
 
 export const isMorningCheckedIn = (morning?: MorningCheckin) => {
   return (
-    !morning ||
-    Number.isFinite(morning.bodyState) ||
-    Number.isFinite(morning.contactsExpected) ||
-    Number.isFinite(morning.mentalState) ||
-    Number.isFinite(morning.sleepLevel) ||
-    Number.isFinite(morning.resourceLevel)
+    morning &&
+    (Number.isFinite(morning.bodyState) ||
+      Number.isFinite(morning.contactsExpected) ||
+      Number.isFinite(morning.mentalState) ||
+      Number.isFinite(morning.sleepLevel) ||
+      Number.isFinite(morning.resourceLevel))
   );
 };
